5699de9a4ea65aed6c3eef5571f6b54d98a82a68
Brian Gilbank <bg@briangilbank.com>
2018-08-19 12:18:56 -0600

    subscribing our customer in stripe

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,12 +1,22 @@
 class SubscriptionsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def new
   end
 
   def create
     customer = current_user.stripe_customer
-    render action: :new
-  end
-  
+    subscription = customer.subscriptions.create(
+      source: params[:stripeToken],
+      plan: params[:plan],
+    )
+
+    current_user.assign_attributes(stripe_subscription_id: subscription.id)
+    current_user.save
+
+    flash.notice = "Thanks for subscribing!"
+    redirect_to root_path
+
+  end  
+
 end
